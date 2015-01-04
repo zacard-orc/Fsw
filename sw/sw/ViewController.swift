529b9b98f724db8df4c3bb48fa1bc83bd7c4865c
z30 <z30@z30deMac.local>
2015-01-04 05:04:06 -0800

    new swift

---
+++ sw/sw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sw
+//
+//  Created by z30 on 15-1-4.
+//  Copyright (c) 2015年 z30. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
